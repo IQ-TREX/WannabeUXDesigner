2c7cf177eda6fae764ce265510b037193f5a4a44
IQ-TREX <76251834+IQ-TREX@users.noreply.github.com>
2023-06-02 01:22:26 +1000

    linking everything and creating sigin page

--- script.js
+++ script.js
@@ -34,3 +34,15 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
+// Sigin Page
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
