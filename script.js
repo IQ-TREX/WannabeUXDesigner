2521662ff9f75a67395bf2e41016a0813f386584
janinenamerian <janine.namerian@gmail.com>
2023-06-03 17:45:02 +1000

    accordion completed

--- script.js
+++ script.js
@@ -13,20 +13,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
-
-accordionItemHeaders.forEach(accordionItemHeader => {
-  accordionItemHeader.addEventListener("click", event => {
-    accordionItemHeader.classList.toggle("active");
-    const accordionItemBody = accordionItemHeader.nextElementSibling;
-    if(accordionItemHeader.classList.contains("active")) {
-      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-    }
-    else {
-      accordionItemBody.style.maxHeight = 0;
-    }
-  });
-});
 
 document.addEventListener('DOMContentLoaded', function() {
     var buttons = document.querySelectorAll('.small-button, .medium-button, .large-button');
