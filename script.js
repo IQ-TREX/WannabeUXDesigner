d9dbeba2ebfa8a7cf74caae4d1a08213b754c2ce
janinenamerian <janine.namerian@gmail.com>
2023-06-03 13:48:10 +1000

    further changes to accordion

--- script.js
+++ script.js
@@ -13,12 +13,18 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elements = document.querySelectorAll('.plus-sign-one, .plus-sign-two, .plus-sign-three, .plus-sign-four, .plus-sign-five');
-    elements.forEach (function(element){
-    element.addEventListener('click', function() {
-      this.classList.toggle('rotate');
-    });
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+  accordionItemHeader.addEventListener("click", event => {
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if(accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    }
+    else {
+      accordionItemBody.style.maxHeight = 0;
+    }
   });
 });
 
